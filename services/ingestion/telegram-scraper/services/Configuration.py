cc8512606ed363f7f513e04e547899a7037e3282
Ishay Sela <ishaisela@gmail.com>
2026-03-13 17:43:45 +0200

    Added the openai api key to the configuration file

--- services/ingestion/telegram-scraper/services/Configuration.py
+++ services/ingestion/telegram-scraper/services/Configuration.py
@@ -6,9 +6,11 @@ from dotenv import load_dotenv
 
 @dataclass
 class TelegramScraperConfig:
-    def __init__(self, api_id: str, api_hash: str):
+    def __init__(self, api_id: str, api_hash: str, openai_api_key: str):
         self.api_id = api_id
         self.api_hash = api_hash
+        self.openai_api_key = openai_api_key
+    __configSingleton: 'None | TelegramScraperConfig' = None
     
     @staticmethod
     def load_from_env() -> 'TelegramScraperConfig':
@@ -20,12 +22,16 @@ class TelegramScraperConfig:
         Returns:
             TelegramScraperConfig: The loaded config from env
         """
+        
+        if TelegramScraperConfig.__configSingleton is not None:
+            return TelegramScraperConfig.__configSingleton
+        
         load_dotenv()
+        openai_api_key = os.environ.get('OPENAI_API_KEY')
+        telegram_api_id = os.environ.get('TELEGRAM_API_ID')
+        telegram_api_hash = os.environ.get('TELEGRAM_API_HASH')
         
-        api_id = os.environ.get('TELEGRAM_API_ID')
-        api_hash = os.environ.get('TELEGRAM_API_HASH')
-        
-        if api_id is None or api_hash is None:
+        if telegram_api_id is None or telegram_api_hash is None or openai_api_key is None:
             raise ValueError('TELEGRAM_API_ID and TELEGRAM_API_HASH must be set')
         
-        return TelegramScraperConfig(api_id=api_id, api_hash=api_hash)
+        return TelegramScraperConfig(api_id=telegram_api_id, api_hash=telegram_api_hash, openai_api_key=openai_api_key)
