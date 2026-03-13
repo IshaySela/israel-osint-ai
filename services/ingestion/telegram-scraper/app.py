5c628839288554d1fad084d538ee5569b18a6856
Ishay Sela <ishaisela@gmail.com>
2026-03-13 19:07:35 +0200

    Moved the publishing logic to a seprate class

--- services/ingestion/telegram-scraper/app.py
+++ services/ingestion/telegram-scraper/app.py
@@ -1,11 +1,10 @@
 from services.Configuration import TelegramScraperConfig
 import asyncio
-import json
 from pyrogram import filters
 from pyrogram.client import Client
 from pyrogram.types import Message
 from services.ClassifyTelegramMessage import classify_telegram_msg
-import pika
+from services.MessageBroker import MessageBroker
 
 
 CONFIG = TelegramScraperConfig.get()
@@ -14,17 +13,14 @@ TZOFAR_TEST_CHANNEL = -1001436772127
 MY_TEST_CHANNEL = -1003756841569
 client = Client("israel-osint-ai-telegram", CONFIG.api_id, CONFIG.api_hash)
 
-# RabbitMQ setup
-connection = pika.BlockingConnection(pika.ConnectionParameters(host=CONFIG.rabbit_host))
-channel = connection.channel()
-channel.queue_declare(queue=CONFIG.rabbit_queue)
+broker = MessageBroker(CONFIG.rabbit_host, CONFIG.rabbit_queue)
 
 @client.on_message(filters.channel & filters.chat([LIVE_TEST_CHANNEL, TZOFAR_TEST_CHANNEL, MY_TEST_CHANNEL]))
 async def listen_for_messages(client: Client, message: Message):
     text = f"{message.caption or ''} {message.text or ''}"
     event_type = await classify_telegram_msg(text)
     print('Received msg', {'text': text, 'event_type': event_type})
-
+    
     if event_type != 'not_relevant':
         event_data = {
             'text': text,
@@ -33,11 +29,7 @@ async def listen_for_messages(client: Client, message: Message):
             'message_id': message.id,
             'date': str(message.date)
         }
-        channel.basic_publish(
-            exchange='',
-            routing_key=CONFIG.rabbit_queue,
-            body=json.dumps(event_data)
-        )
+        broker.publish_event(event_data)
         print(f"Published event: {event_type}")
-
+    
 client.run()
