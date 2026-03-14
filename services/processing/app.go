a0559357c50ff53b41e1e8105669a1ffe53203a0
IshaySela <51655701+IshaySela@users.noreply.github.com>
2026-03-14 00:12:19 +0000

    implemented a basic queue listener

--- services/processing/app.go
+++ services/processing/app.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	MessageQueue "github.com/IshaySela/israel-osint-ai/services/processing/messagebroker"
+	models "github.com/IshaySela/israel-osint-ai/services/processing/models"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	broker := MessageQueue.NewRabbitListener("amqp://guest:guest@localhost:5672/", "osint_events")
+
+	fmt.Println("Starting message broker...")
+	done := make(chan bool)
+
+	err := broker.Listen(func(event models.RawOsintEvent) {
+		fmt.Printf("Received event: %s\n", string(event.Text))
+	})
+
+	if err != nil {
+		fmt.Printf("Error starting message broker: %v\n", err)
+	}
+
+	<-done
 }
