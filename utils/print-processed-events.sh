d8fffa7d661aa924ee0f738eac7f13cb9d7e60e5
Ishay Sela <ishaisela@gmail.com>
2026-04-13 18:25:46 +0300

    chore: add utils scripts

---
+++ utils/print-processed-events.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if command -v amqp-consume > /dev/null 2>&1; then
+    amqp-consume \
+    --url="amqp://guest:guest@localhost:5672" \
+    --exchange="processed_events" --routing-key="#" \
+    cat
+else
+    echo "Please install amqp-tools"
+fi
