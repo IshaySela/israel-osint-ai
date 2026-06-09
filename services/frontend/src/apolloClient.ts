00b61780a4ae7d238f5d84c5cb2ebb3e76fd7106
IshaySela <51655701+IshaySela@users.noreply.github.com>
2026-06-09 16:11:34 +0300

    Added an ENV variable to config the backend URL in the frontend (#63)
    added an ENV variable to load VITE_BACKEND_URL to the application, defaults to localhost on development
    
    Updated the hardcoded string to use the new config
    Updated docker-compose.yaml to optionally load .env file from the frontend folder

--- services/frontend/src/apolloClient.ts
+++ services/frontend/src/apolloClient.ts
@@ -1,7 +1,8 @@
 import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
+import config from './store/config';
 
 const client = new ApolloClient({
-  link: new HttpLink({ uri: 'http://localhost:5000/graphql' }),
+  link: new HttpLink({ uri: `${config.BACKEND_URL}/graphql` }),
   cache: new InMemoryCache(),
 });
 
